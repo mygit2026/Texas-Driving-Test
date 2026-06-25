2c3402c50088e5f025d902cc7b419e3031a3f188
mygit2026 <119744777+mygit2026@users.noreply.github.com>
2026-06-24 22:33:35 -0500

    Add first quiz question

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
+const questionArea = `
+<h2>Question 1 of 150</h2>
+
+<p>
+What does a red octagonal sign mean?
+</p>
+
+<label>
+<input type="radio" name="q1">
+Yield
+</label>
+
+<br><br>
+
+<label>
+<input type="radio" name="q1">
+Railroad Crossing
+</label>
+
+<br><br>
+
+<label>
+<input type="radio" name="q1">
+Stop
+</label>
+
+<br><br>
+
+<label>
+<input type="radio" name="q1">
+School Zone
+</label>
+
+<br><br>
+
+<label>
+<input type="radio" name="q1">
+No Passing
+</label>
+
+<br><br>
+
+<button class="btn">
+Next Question
+</button>
+`;
+
 function startStudyMode() {
-    alert("Study Mode Coming Soon");
+    document.querySelector(".container").innerHTML = questionArea;
 }
 
 function startExamMode() {
-    alert("Exam Mode Coming Soon");
+    document.querySelector(".container").innerHTML = questionArea;
 }
